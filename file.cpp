b6801ddbcdf8cae1b26c081b2c14fc7ea4c5e465
Пользователь <you@example.com>
2020-03-05 08:58:43 +0000

    my first commit

---
+++ file.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    int i = 0;
+    while(i < argc)
+    {
+        printf("%s\n", argv[i]);
+        i++;
+    }
+    return 0;
+}
+// Hello world. Num is 45
+
+
